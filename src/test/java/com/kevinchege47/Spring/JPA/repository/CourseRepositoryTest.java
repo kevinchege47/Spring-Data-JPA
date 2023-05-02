04f29cd4d43d012ecc017b1b35660c2acf55013c
kevinchege47 <chegekevin001@gmail.com>
2023-05-02 13:49:56 +0300

    many to many relationship

--- src/test/java/com/kevinchege47/Spring/JPA/repository/CourseRepositoryTest.java
+++ src/test/java/com/kevinchege47/Spring/JPA/repository/CourseRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.kevinchege47.Spring.JPA.repository;
+
 import com.kevinchege47.Spring.JPA.entity.Course;
+import com.kevinchege47.Spring.JPA.entity.Student;
 import com.kevinchege47.Spring.JPA.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -11,13 +15,15 @@ import java.util.List;
 class CourseRepositoryTest {
     @Autowired
     private CourseRepository courseRepository;
+
     @Test
-    public void getCourses(){
+    public void getCourses() {
         List<Course> courseList = courseRepository.findAll();
         System.out.println("courseList = " + courseList);
     }
+
     @Test
-    public void saveCourseWithTeacher(){
+    public void saveCourseWithTeacher() {
         Teacher teacher = Teacher.builder()
                 .firstName("Amigo")
                 .lastName("G")
@@ -27,6 +33,35 @@ class CourseRepositoryTest {
                 .credit(2)
                 .teacher(teacher)
                 .build();
+        courseRepository.save(course);
+    }
+
+    @Test
+    public void findAllPagination() {
+        Pageable firstPagewithThreeRecords = PageRequest.of(0,3);
+        Pageable secondPageWithTwoRecords = PageRequest.of(1,2);
+        List<Course> courses = courseRepository.findAll(firstPagewithThreeRecords).getContent();
+        long totalElements = courseRepository.findAll(firstPagewithThreeRecords).getTotalElements();
+        long totalPages = courseRepository.findAll(firstPagewithThreeRecords).getTotalPages();
+
+    }
+    @Test
+    public void saveCourseWithStudentAndTeacher(){
+        Student student = Student.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .emailId("ggg@gmail.com")
+                .build();
+        Teacher teacher = Teacher.builder()
+                .firstName("Mary")
+                .lastName("Jane")
+                .build();
+        Course course = Course.builder()
+                .title("AI")
+                .credit(12)
+                .teacher(teacher)
+                .build();
+        course.addStudents(student);
         courseRepository.save(course);
     }
 
